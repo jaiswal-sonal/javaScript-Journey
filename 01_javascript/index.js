a4f740906d687022a0715d975c855d8a85d37b48
jaiswal-sonal <jaiswalsonal338@gmail.com>
2026-09-23 09:41:57 -0700

    today done

--- 01_javascript/index.js
+++ 01_javascript/index.js
@@ -1,2 +1,27 @@
-console.log("JavaScript is working!");
-console.log("hi");
+
+function employee() {
+    let salary = 5000;
+
+    if(salary > 4000) {
+        let bonus = 500;
+    } // let respect to block
+
+    // console.log(bonus);
+} // let respect to function scope ;
+
+console.log(salary);
+
+employee();
+
+function employee2(){
+
+    var salary = 3000;
+
+    if(salary > 2000) {
+        var bonus = 20; 
+    } // var does not respect block
+    // console.log(bonus);
+} // var give respect to function 
+
+console.log(salary)
+employee2();
